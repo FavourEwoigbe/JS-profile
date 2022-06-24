a7a85c7ec077fff818f9c5b45e394eb60612e03e
Favour Ewoigbe <79311438+FavourEwoigbe@users.noreply.github.com>
2022-06-24 08:16:45 +0100

    Create Physical.js

---
+++ Physical.js
@@ -0,0 +1,7 @@
+let aeroplane  = prompt("Name?");
+let cockpit = prompt("Height?");
+let fuselage = prompt("Country?");
+document.getElementById("name").innerHTML= aeroplane;
+document.getElementById("height").innerHTML=cockpit;
+document.getElementById("country").innerHTML=fuselage;
+document.getElementById("Aerospace").style.display="block";
